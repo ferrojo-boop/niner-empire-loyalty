3ccc71184d45be7ff3be86b076a36ef9940c3695
ferrojo-boop <fer.rojo@canvasmediamx.com>
2026-06-30 12:36:07 -0600

    fix: remove unused useRef import

--- app/tarjeta/[fanId]/page.tsx
+++ app/tarjeta/[fanId]/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'next/navigation'
 import { FanCard } from '@/components/FanCard'
 import QRCode from 'qrcode'
